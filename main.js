eda849bc14f5212f48640f795f9b286215e0f4b2
tuananh171101 <101715284+tuananh171101@users.noreply.github.com>
2023-06-30 13:12:30 +0700

    update code

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((n) => n.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
